cee5d56028592f0735be98c37ce9390465fea3d6
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2024-05-30 14:37:05 +0700

    feat: frame default layout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,16 +1,21 @@
+import classNames from "classnames/bind";
 import Header from "~/components/Layout/components/Header";
+import styles from './DefaultLayout.module.scss'
 import Sidebar from "./SideBar";
 
+const cx = classNames.bind(styles)
+
 function DefaultLayout({ children }) {
-    return (<div>
-        <Header />
-        <div className="container">
-            <Sidebar />
-            <div className="content">
-                {children}
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>
+                    {children}
+                </div>
             </div>
-        </div>
-    </div>)
+        </div>)
 }
 
 export default DefaultLayout;
