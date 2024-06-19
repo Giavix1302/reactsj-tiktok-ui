543124156ac5e4e8c0a5bda1ed9ba7f9fd898a82
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2024-06-19 17:20:26 +0700

    perf: Optimize Header

---
+++ src/components/Popper/Menu/Menu.js
@@ -0,0 +1,72 @@
+import Tippy from "@tippyjs/react/headless";
+import PropTypes from 'prop-types'
+
+import classNames from "classnames/bind";
+import styles from './Menu.module.scss'
+
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import MenuItem from "./MenuItem";
+import Header from './Header'
+import { useState } from "react";
+
+const cx = classNames.bind(styles)
+const defaultFN = () => { }
+
+function Menu({ children, items = [], hideOnClick = false, onChange = defaultFN }) {
+
+    const [history, setHistory] = useState([{ data: items }])
+
+    const current = history[history.length - 1]
+
+    const renderItems = () => {
+        return current.data.map((item, index) => {
+            const isParent = !!item.children
+            return <MenuItem key={index} data={item} onClick={() => {
+                if (isParent) {
+                    setHistory(prev => [...prev, item.children])
+                } else {
+                    onChange(item)
+                }
+            }} />
+        })
+    }
+
+    const handleBack = () => {
+        setHistory(prev => prev.slice(0, prev.length - 1))
+    }
+
+    const renderResult = (attrs) => (
+        <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+            <PopperWrapper className={cx('menu-popper')}>
+                {history.length > 1 && <Header title={current.title} onBack={handleBack} />}
+                <div className={cx('menu-body')}>{renderItems()}</div>
+            </PopperWrapper>
+        </div>
+    )
+    // Reset to first page
+    const handleReset = () => {
+        setHistory(prev => prev.slice(0, 1))
+    }
+
+    return (
+        <Tippy
+            offset={[12, 8]}
+            interactive
+            hideOnClick={hideOnClick}
+            placement='bottom-end'
+            render={renderResult}
+            onHide={handleReset}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+Menu.propTypes = {
+    children: PropTypes.node.isRequired,
+    items: PropTypes.array,
+    hideOnClick: PropTypes.bool,
+    onChange: PropTypes.func,
+}
+
+export default Menu;
